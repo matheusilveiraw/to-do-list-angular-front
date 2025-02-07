b0d691b903da6e57fb7124c9899549b8ed777308
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-02-06 21:31:58 -0300

    front recebendo o back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  message: string = '';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getMessage().subscribe({
+      next: (data) => {
+        this.message = data.message;
+      },
+      error: (error) => {
+        console.error('Erro ao buscar mensagem:', error);
+      }
+    });
+  }
 }
