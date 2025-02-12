43f586d2d64cdcaa046fa52f229a54ed8bbc50ed
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-02-12 19:15:04 -0300

    instalado tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}",  //para o tailwind funcionar no angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
